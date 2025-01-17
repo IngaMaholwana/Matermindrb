ad7f533dee7a6142142f6ee041ddf1f105b34744
Inga Maholwana <imaholwana023@student.wethinkcode.co.za>
2025-01-17 21:18:15 +0200

    codebreaker part logic coming along done

--- codebreaker.rb
+++ codebreaker.rb
@@ -2,7 +2,7 @@
 class Codebreaker 
   def initialize
     @turns = 3 #10 # player has this many round to get the secret code right
-  
+    @guesses = [] # player guesses will be stored here
   end
 
   def play
@@ -12,7 +12,17 @@ class Codebreaker
     puts "You have #{@turns} turns. Good luck!"
 
     while @turns > 0
-      print "turn #{4 - turns}: Enter your guess  code breaker"
+      print "turn #{4 - @turns}: Enter your guess  code breaker  "
+      input = gets.chomp.split(",").map(&:strip)
+      if input.size == 4
+        @guesses << input
+        @turns -= 1
+        puts "You have set your guess set: #{@guesses.join(', ')}"
+        break
+      else
+        puts "Invalid input. Please enter 4 colors separated by commas."
+      end
+    
     end
   end
 
